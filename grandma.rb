deaa8541be1124a01c5bcc7bbb17f4b17efd4d75
Saudia Schwegler <saudiaschwegler@gmail.com>
2020-08-23 02:17:44 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -3,8 +3,8 @@ def speak_to_grandma(phrase)
 # Whatever you say to grandma, she should respond with
 # HUH?! SPEAK UP, SONNY!
 # unless you shout it (type in all capitals).
-    if phrase == "I LOVE YOU GRANDMA"
-      return "I LOVE YOU TOO PUMPKIN"
+    if phrase == "I LOVE YOU GRANDMA!"
+      return "I LOVE YOU TOO PUMPKIN!"
 # If you shout, she can hear you (or at least she thinks so)
 # and yells back
     elsif phrase == phrase.downcase
