63e645c01d4d57bbd431c48058d19deedd57103c
Saudia Schwegler <saudiaschwegler@gmail.com>
2020-08-23 02:17:15 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,6 +1,5 @@
 # Write a speak_to_grandma method.
 def speak_to_grandma(phrase)
-  HUH?! SPEAK UP, SONNY!
 # Whatever you say to grandma, she should respond with
 # HUH?! SPEAK UP, SONNY!
 # unless you shout it (type in all capitals).
