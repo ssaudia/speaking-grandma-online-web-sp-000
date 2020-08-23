30961e4cd8b3770b096676db62e653c2d541b2d1
Saudia Schwegler <saudiaschwegler@gmail.com>
2020-08-23 02:22:16 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -15,7 +15,7 @@ def speak_to_grandma(phrase)
       return "HUH?! SPEAK UP, SONNY!"
 # NO, NOT SINCE 1938!
     else
-      return "HUH?! SPEAK UP, SONNY!"
+      return "NO, NOT SINCE 1938!"
     end
 # However if you say 'I LOVE YOU GRANDMA!', she should respond with
 # 'I LOVE YOU TOO PUMPKIN!'
